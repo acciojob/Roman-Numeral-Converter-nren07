9f1e20464592402189ec5480c6fc7068465c3606
NARENDRA SINGH <117838092+nren07@users.noreply.github.com>
2024-01-18 17:22:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+  	const obj = [
+		['M',1000], 
+		['CM',900],
+		['D', 500], 
+		['CD',400],
+	    ['C', 100],
+		['XC',90],
+	    ['L', 50],
+		['XL',40],
+	    ['X', 10], 
+		['IX',9],
+		['V', 5], 
+		['IV',4],
+	    ['I', 1]
+    ];
+	
+	let roman="";
 
-  //your code here
+	while(num!=0){
+		let returnArr=function(num){
+			for(let i=obj.length-1;i>=0;i--){
+				if(num==obj[i][1]) return obj[i];
+				if(num<obj[i][1]) return obj[i-1];
+			}
+			return obj[obj.length-1];
+		};
+		roman+=returnArr[0];
+		num-=returnArr[1];
+	}
+	return roman;
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
+console.log(convertToRoman(36));
 
 
 
